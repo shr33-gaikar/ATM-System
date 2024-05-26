90d6848602b6387af3f8e0dfbf72289acacd1f35
Shrikar Gaikar <shreekarg30@gmail.com>
2024-05-26 14:21:24 +0530

    first commit

---
+++ src/bank/management/system/Connection.java
@@ -0,0 +1,18 @@
+package bank.management.system;
+
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Connection {
+    java.sql.Connection connection;
+    Statement statement;
+
+    public Connection() {
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankSystem", "root", "shree@mysql");
+            statement = connection.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
